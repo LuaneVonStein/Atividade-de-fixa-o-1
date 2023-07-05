a8088a2073921cf82e57c4fd70f5ca09babfb622
Luiz F. Picolo <luizpicolo@gmail.com>
2023-07-05 17:16:42 -0400

    exercícios

---
+++ Script.js
@@ -0,0 +1,39 @@
+// Exercício 4
+
+let valor = 10;
+
+if(valor % 2 === 0) {
+    console.log("O número informado é par");
+}
+
+else{
+    console.log("O número informado é impar");
+}
+
+//Exercício 5
+
+let numero = 16;
+
+if(numero > 0) {
+console.log("O número informado é positivo");
+}
+
+else{
+console.log("O número informado é negativo");
+}
+
+//Exercício 9
+let contador  = 1;
+
+while (contador <=10) {
+    console.log(contador);
+    if(valor % 2 === 0) {
+        console.log("Somente números pares");
+    }
+    
+    else{
+        console.log("");
+    }
+    
+    contador++;
+}
