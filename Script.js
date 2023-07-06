3c671d5210e08f60e79eef64040a617dcc6df669
Lorrana Damasceno <131817254+Lolozix@users.noreply.github.com>
2023-07-06 16:52:54 -0400

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,3 +1,53 @@
+//Exercício 1
+let letra = 1;
+
+if ((letra % 4 === 0 && letra % 100 !== 0) || (letra % 400 === 0)) {
+  console.log("A letra indicada é uma consoante");
+} else {
+  console.log("A letra indicada é uma vogal");
+}
+
+
+//Exercício 2
+let valor nota = 10
+
+if (nota === 50) {
+  console.log("a")
+} 
+
+else if (nota === 60) {
+    console.log("b")
+} 
+
+else if (nota === 70) {
+    console.log("c")
+}
+
+else if (nota === 80) {
+    console.log("d")
+}
+
+else if (nota === 90) {
+    console.log("e")
+}
+
+else if (nota === 100) {
+    console.log("f")
+}
+
+
+//Exercício 3
+let valor = 0
+
+if (valor % 3 === 0 && valor % 5 === 0 ) {
+  console.log("O número" + valor + " é divisível por 3 ou por 5");
+} 
+
+else {
+  console.log("O número" + valor + " não é divisível por 3 ou por 5");
+}
+
+
 // Exercício 4
 
 let valor = 10;
@@ -10,6 +60,7 @@ else{
     console.log("O número informado é impar");
 }
 
+
 //Exercício 5
 
 let numero = 16;
@@ -21,6 +72,38 @@ console.log("O número informado é positivo");
 else{
 console.log("O número informado é negativo");
 }
+
+
+//Exercício 6
+let contador = 100;
+
+while(contador <= 1){
+    console.log(contador);
+    contador--;
+    const numero = [1, 2, 3, 4, 5];
+
+for(let number of numero){
+        console.log(number)
+    }
+}    
+
+
+//Exercício 7
+const numero = [a, b, c];
+
+for(let number of numero){
+    console.log(number);
+}
+
+
+//Exercício 8
+let divisível = 0
+
+for(let i = 1; i <= 10; i++){
+    soma + = i;
+}
+console.log("O valor da soma é: " + soma");
+
 
 //Exercício 9
 let contador  = 1;
@@ -37,3 +120,82 @@ while (contador <=10) {
     
     contador++;
 }
+
+
+//Exercício 10
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for(let num of numeros){
+  console.log(num);
+}
+
+
+//Exercício 11
+let soma = 0;
+
+for(let i = 1; i >= 2; && >=3; && >=5; i++){
+  soma += i;
+    console.log("O número é divisível por: " + soma);
+}
+
+
+//Exercício 12
+const num5 = [10, 20, 30, 40];
+
+for (let [indice, num5] of num5.entries() ){
+    console.log("numero" + num5 + "na posição" + indice);
+}
+
+
+//Exercício 13
+let contador2 = 2;
+
+while(contador2 <= )
+
+
+//Exercício 14
+let num4;
+
+do {
+  num4 = parseInt(prompt("Digite um número entre 1 e 10"));
+} 
+    
+while ();
+
+//Exercício 15
+const number = 
+
+
+//Exercício 16
+
+
+
+//Exercício 17
+
+
+
+//Exercício 18
+let numero1;
+
+do {
+  numero = parseInt(prompt("Digite dois números inteiros"));
+} 
+while (numero1 < 1 || numero1 > 100);
+
+if(valor&2 === 0) {    
+}
+console.log("Você digitou o número " + numero1);
+
+
+//Execício 19
+let notas;
+    do {
+    nota = parseInt(prompt("Digite dois números inteiros"));    
+}
+    while (notas < 1 || nor=tas > 10; i++) {
+        let soma = 0;
+    }
+console.log("O valor da soma é: " + soma);
+
+
+//Exercício 20
